d18cf082ed7996c8a75ac750020b87f7a65c41fd
TomisTony <1158176767@qq.com>
2022-12-04 13:35:09 +0800

    now commits data no longer = 234

--- client/src/components/DashBoard/CommitNumber.js
+++ client/src/components/DashBoard/CommitNumber.js
@@ -4,39 +4,43 @@ import { Card, Typography } from "@mui/material";
 import { fShortenNumber } from "../../utils/formatNumber";
 
 const RootStyle = styled(Card)(({ theme }) => ({
-  boxShadow: "none",
-  textAlign: "center",
-  padding: theme.spacing(5, 0),
-  color: theme.palette.error.dark,
-  backgroundColor: theme.palette.error.lighter,
+    boxShadow: "none",
+    textAlign: "center",
+    padding: theme.spacing(5, 0),
+    color: theme.palette.error.dark,
+    backgroundColor: theme.palette.error.lighter,
 }));
 
 const IconWrapperStyle = styled("div")(({ theme }) => ({
-  margin: "auto",
-  display: "flex",
-  borderRadius: "50%",
-  alignItems: "center",
-  width: theme.spacing(8),
-  height: theme.spacing(8),
-  justifyContent: "center",
-  marginBottom: theme.spacing(3),
-  color: theme.palette.error.dark,
-  backgroundImage: `linear-gradient(135deg, ${alpha(
-    theme.palette.error.dark,
-    0
-  )} 0%, ${alpha(theme.palette.error.dark, 0.24)} 100%)`,
+    margin: "auto",
+    display: "flex",
+    borderRadius: "50%",
+    alignItems: "center",
+    width: theme.spacing(8),
+    height: theme.spacing(8),
+    justifyContent: "center",
+    marginBottom: theme.spacing(3),
+    color: theme.palette.error.dark,
+    backgroundImage: `linear-gradient(135deg, ${alpha(
+        theme.palette.error.dark,
+        0
+    )} 0%, ${alpha(theme.palette.error.dark, 0.24)} 100%)`,
 }));
 
-const TOTAL = 234;
 
-export default function CommitNumber() {
-  return (
-    <RootStyle>
-      <IconWrapperStyle>
-        <Icon icon="bx:bx-git-commit" width={24} height={24} />
-      </IconWrapperStyle>
-      <Typography variant="h3">{fShortenNumber(TOTAL)}</Typography>
-      <Typography variant="subtitle2">Commits</Typography>
-    </RootStyle>
-  );
+export default function CommitNumber(data) {
+    let total = 0;
+    let commits = data.commit_month_frequency;
+    for(var i in commits){
+        total += commits[i];
+    }
+    return (
+        <RootStyle>
+            <IconWrapperStyle>
+                <Icon icon="bx:bx-git-commit" width={24} height={24} />
+            </IconWrapperStyle>
+            <Typography variant="h3">{fShortenNumber(total)}</Typography>
+            <Typography variant="subtitle2">Commits</Typography>
+        </RootStyle>
+    );
 }
--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -124,7 +124,7 @@ export default function DashboardApp() {
                 <Box>
                     <Grid container spacing={3}>
                         <Grid item xs={12} sm={6} md={3}>
-                            <CommitNumber />
+                            <CommitNumber commit_month_frequency={commit_month_frequency} />
                         </Grid>
                         <Grid item xs={12} sm={6} md={3}>
                             <IssueNumber total={open_issues} />
