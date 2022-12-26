2bba4d6b4c0c3d0e4341ad86b41b7d14c5b0e849
IDEA <3200105872@zju.edu.cn>
2022-12-26 15:55:23 +0800

    add db data

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,15 @@
 var config = {
 
-    DEBUG: false,
+    DEBUG: true,
 
-    MONGO_URI: "mongodb://120.26.195.57:27017/DoubleC",
+    // MONGO_URI: "mongodb://120.26.195.57:27017/DoubleC",
 
-    // MONGO_URI: "mongodb://beet.asia:27017/doublec_pytorch?directConnection=true",
+    MONGO_URI: "mongodb://beet.asia:27017/doublec_pytorch?directConnection=true",
 
-    GITHUB_ACCESS_TOKEN: "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"
+    // GITHUB_ACCESS_TOKEN: "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",
+    GITHUB_ACCESS_TOKEN: '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',
+    // GITHUB_ACCESS_TOKEN: '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',
+    // GITHUB_ACCESS_TOKEN: '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'
 };
 
 module.exports = config;
